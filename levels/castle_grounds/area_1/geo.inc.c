6161ef7ced52d0308c84ea9c8e1e8769ffe25f82
someone2639 <someone2639@gmail.com>
2023-12-21 18:11:47 -0500

    fix camera bug at heights

--- levels/castle_grounds/area_1/geo.inc.c
+++ levels/castle_grounds/area_1/geo.inc.c
@@ -3,7 +3,8 @@
 const GeoLayout castle_grounds_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, castle_grounds_dl_Cylinder_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 0, -10808, 0, castle_grounds_dl_Cylinder_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 0, 10515, 0, castle_grounds_dl_Cylinder_001_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
--- src/game/camera.c
+++ src/game/camera.c
@@ -1073,19 +1073,19 @@ void mode_8_directions_camera(struct Camera *c) {
     f32 dist;
 
     Vec3f z = {0,0,0};
+    z[1] = gMarioState->pos[1];
     vec3f_get_dist_and_angle(gMarioState->pos, z, &dist, &pitch, &yaw);
 
     vec3f_set_dist_and_angle(z, c->pos, 2000.0f, pitch, yaw + 0x8000);
 
     c->focus[0] = 0;
     c->focus[2] = 0;
-    c->focus[1] = 0;
+    c->focus[1] = gMarioState->pos[1];
 
     c->nextYaw = yaw + 0x8000;
 
 
     c->pos[1] = gMarioState->pos[1] + 500.0f;
-    // set_camera_height(c, gMarioState->pos[1] + 500.0f);
 }
 
 /**
